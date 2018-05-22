b85c3ad47cacb9fd59ec8d1b4d8e2518c4a3a5ed
renguyen <reginanguyen01@gmail.com>
2018-05-21 19:08:08 -0700

    added code to read training data

--- baseline.py
+++ baseline.py
@@ -1,3 +1,26 @@
+import os
+import csv
+import json
 from sklearn.linear_model import LogisticRegression
+import pdb
 
+data_home = 'data'
+base_data_filename = os.path.join(data_home, 'training_Set_rel3.tsv')
+
+# Data will be stored as a dictionary where:
+# key(string)   = '{essay_id},{essay_set}'
+# value(dict)   = { 'text': the ascii text of a student's response,
+#                   'score': the average grader score }
+data = {}
+
+with open(base_data_filename, 'rt') as f:
+  for line in csv.reader(f, delimiter='\t'):
+    key = line[0] + ',' + line[1]
+    score = 0
+    if line[1] is '8' and line[5] is not '':    # some essays in set 8 have a third score
+      score = (int(line[3]) + int(line[4]) + int(line[5])) / 3.0
+    else:
+      score = (int(line[3]) + int(line[4])) / 2.0
+    value = { 'text': line[2], 'score': score }
+    data[key] = value
 

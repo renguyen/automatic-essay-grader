7c26bff4a1f495d0fc937d8739a34db536a941c8
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2018-05-28 16:45:33 -0700

    Changed to Cohen's Kappa

--- rnn.py
+++ rnn.py
@@ -93,7 +93,7 @@ def report(results, n_top=3):
         candidates = np.flatnonzero(results['rank_test_score'] == i)
         for candidate in candidates:
             print("Model with rank: {0}".format(i))
-            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+            print("Mean Cohen Kappa score: {0:.3f} (std: {1:.3f})".format(
                   results['mean_test_score'][candidate],
                   results['std_test_score'][candidate]))
             print("Parameters: {0}".format(results['params'][candidate]))
